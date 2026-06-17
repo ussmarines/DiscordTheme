19895b276ab8cf5025e077cae6b65b8395c6fb9d
ussmarines <25757331+ussmarines@users.noreply.github.com>
2026-06-17 19:32:53 +0200

    commit

--- test/smoke-check.js
+++
@@ -1,3 +0,0 @@
-const { execFileSync } = require('child_process');
-execFileSync(process.execPath, ['scripts/build.js'], { stdio: 'inherit' });
-execFileSync(process.execPath, ['scripts/check.js'], { stdio: 'inherit' });
